7a9ac3d2e62319812476ed2a104f9baed697285d
yichen <1097179511@qq.com>
2022-06-14 10:38:46 +0800

    fix bug
    fix bug and add usages

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MQTT_Security_Tool
 对MQTT的broker匿名登陆、以及账户密码进行爆破
+
+## 使用方法
+
+在此处统一修改参数![params](img/params.png)
+
+运行即可
+
+```
+python main.py
+```
+
+## 检测匿名登陆
+
+![unauth](img/unauth.png)
+
+## 检测弱口令
+
+![weakpass](img/weakpass.png)
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import uuid
 from pyfiglet import Figlet
 import os,argparse,sys,time,json
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('--addr', action="store")
-parser.add_argument('--port', action="store", type=int)
-parser.add_argument('--user', action="store")
-parser.add_argument('--passwd', action="store")
+############### params #################
+broker = "192.168.127.137"
+port = 1883
+userfile = "./user.txt"
+passfile = "./pass.txt"
+########################################
 
 usernames = []
 passwords = []
@@ -16,7 +17,7 @@ output = {"unauth":False,"weakpass":False,"username":"","password":""}
 def on_connect_unauth(client, userdata, flags, rc):
     if rc == 0:
         output["unauth"] = True
-        print("发现匿名登陆!!")
+        print("[√]发现匿名登陆!!")
         client.disconnect()
         client.loop_stop()
         return True
@@ -27,9 +28,8 @@ def on_connect_unauth(client, userdata, flags, rc):
 
 def on_connect_brute(client, userdata, flags, rc):
     if rc == 0:
-        global flag
-        flag = True
-        print("暴力破解连接成功！")
+        output["weakpass"] = True
+        print("[√]暴力破解连接成功！")
         client.disconnect()
         client.loop_stop()
         return True
@@ -57,45 +57,27 @@ def brute(broker, port, userfile, passfile, usernames, passwords):
     for pass_name in pass_file:
         passwords.append(pass_name[:-1])
     
-    client = mqtt_client.Client(str(uuid.uuid4()))
-    client.on_connect = on_connect_brute
     for user in usernames:
         for passwd in passwords:
+            client = mqtt_client.Client(str(uuid.uuid4()))
+            client.on_connect = on_connect_brute
             print("[+]正在测试: username:"+user+" "+"password:"+passwd)
             client.username_pw_set(username=user, password=passwd)
             client.connect(broker, port)
-            client.loop_start()
             time.sleep(0.5)
-            if flag == True:
-                output["weakpass"] = True
+            client.loop_start()
+            if output["weakpass"] == True:
                 output["username"] = user
                 output["password"] = passwd
                 return 
 
-def help_info():
-    print("Usage:")
-    print("     --addr  broker地址")
-    print("     --port  broker端口")
-    print("     --user  用户名字典路径")
-    print("     --passwd  密码字典路径")
-
 f = Figlet(font="slant", width=100)
 print(f.renderText("MQTT Security Tool"))
-try:
-    args = parser.parse_args()
-except:
-    help_info()
-    sys.exit()
-
-broker = args.addr
-port = args.port
-userfile = args.user
-passfile = args.passwd
 
 print("[+]正在测试匿名登陆...")
 unauthaccess(broker, port)
 if (output["unauth"] == False):
-    print("[+]不存在匿名登陆，正在暴力破解用户名和密码...")
+    print("[X]不存在匿名登陆，正在暴力破解用户名和密码...")
     brute(broker, port, userfile, passfile, usernames, passwords)
 data = json.dumps({'result':output})
 print(data)
